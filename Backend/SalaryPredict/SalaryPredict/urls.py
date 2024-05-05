c601a6fa2a0ce265becd061b6c8bedaaae94a1b1
MDalamin5 <mdal.amin5@northsouth.edu>
2024-05-05 18:07:44 +0600

    page setup done

--- Backend/SalaryPredict/SalaryPredict/urls.py
+++ Backend/SalaryPredict/SalaryPredict/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path('predict/', include('predict.urls')),
+    path('', views.home, name='home'),
+    path('predict/', include('predict.urls')),
 ]
--- Backend/SalaryPredict/predict/views.py
+++ Backend/SalaryPredict/predict/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def predict(request):
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'gs.html')
